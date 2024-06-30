5dc18ad29bdd0fb7e5822016170993b018494e41
Shivam Arya <sw.shivamarya@gmail.com>
2024-06-30 19:36:15 +0530

    java program for Factorial calculator using normal approach

---
+++ Java/FactorialNormal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FactorialNormal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a non-negative integer: ");
+        int num = sc.nextInt();
+        if (num < 0) {
+            System.out.println("Error: Factorial is not defined for negative numbers.");
+        } else {
+            long factorial = calculateFactorial(num);
+            System.out.println("Factorial of " + num + " is: " + factorial);
+        }
+        sc.close();
+    }
+    public static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        
+        long factorial = 1;
+        for (int i = 2; i <= n; i++) {
+            factorial *= i;
+        }
+        
+        return factorial;
+    }
+}
